9cdf073ac36cf309d014dc43c7d2f870596fbb84
SzymonWalasik <szymwal050@student.polsl.pl>
2023-04-14 15:42:49 +0200

    stworzenie struktury komponentow

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { CarouselComponent } from './landing-page/carousel/carousel.component';
+import { ContactFormComponent } from './landing-page/contact-form/contact-form.component';
+import { FooterComponent } from './landing-page/footer/footer.component';
+import { InstagramPostsComponent } from './landing-page/instagram-posts/instagram-posts.component';
+import { NavbarComponent } from './landing-page/navbar/navbar.component';
+import { UserLoginStateComponent } from './landing-page/navbar/user-login-state/user-login-state.component';
+import { SearchFormComponent } from './landing-page/search-form/search-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    CarouselComponent,
+    ContactFormComponent,
+    FooterComponent,
+    InstagramPostsComponent,
+    NavbarComponent,
+    UserLoginStateComponent,
+    SearchFormComponent
   ],
   imports: [
     BrowserModule,
