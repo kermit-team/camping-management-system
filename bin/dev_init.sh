5063c59c987cd00c11a94cd6a7ebd1d928100fd4
Patryk Beberok <patrol0990@gmail.com>
2023-04-16 11:51:45 +0200

    Added fixture loading to application

--- bin/dev_init.sh
+++ bin/dev_init.sh
@@ -6,12 +6,12 @@ export DOCKER_BUILDKIT=1
     docker compose up --detach
     docker compose run --rm django python manage.py makemigrations
     docker compose run --rm django python manage.py migrate    
-    #docker compose run --rm django python manage.py loaddata groups_and_permissions.json
+    docker compose run --rm django python manage.py loaddata init_groups_and_permissions.json
     docker compose run --rm django python manage.py createsuperuser --no-input
 } || {
     docker-compose up --detach
     docker-compose run --rm django python manage.py makemigrations
     docker-compose run --rm django python manage.py migrate
-    #docker-compose run --rm django python manage.py loaddata groups_and_permissions.json
+    docker-compose run --rm django python manage.py loaddata init_groups_and_permissions.json
     docker-compose run --rm django python manage.py createsuperuser --no-input
 }
