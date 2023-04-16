6298865781835ccbdd31f362558be3d75c19defb
Patryk Beberok <patrol0990@gmail.com>
2023-04-16 11:47:48 +0200

    Added password validators for uppercase, lowercase and special character recognition

---
+++ backend/account/validators/__init__.py
@@ -0,0 +1,3 @@
+from .lowercase_validator import LowercaseValidator
+from .uppercase_validator import UppercaseValidator
+from .special_character_validator import SpecialCharacterValidator
---
+++ backend/account/validators/special_character_validator.py
@@ -0,0 +1,20 @@
+import re
+
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext as _
+
+
+class SpecialCharacterValidator:
+    def validate(self, password, user=None):
+        if not re.findall('[()[\]{}|\\`~!@#$%^&*_\-+=;:\'",<>./?]', password):
+            raise ValidationError(
+                _("The password must contain at least 1 symbol: " +
+                  "()[]{}|\`~!@#$%^&*_-+=;:'\",<>./?"),
+                code='password_no_symbol',
+            )
+
+    def get_help_text(self):
+        return _(
+            "Your password must contain at least 1 symbol: " +
+            "()[]{}|\`~!@#$%^&*_-+=;:'\",<>./?"
+        )
