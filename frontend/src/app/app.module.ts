ddf5c382e52cb2bd212dd18eaa8497837515c0d8
Katarzyna Szczepańczyk <k.szczepanczyk83@gmail.com>
2023-04-18 23:45:21 +0200

    Added login page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,26 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LandingPageComponent } from './landing-page/landing-page.component';
-import { CarouselComponent } from './landing-page/carousel/carousel.component';
-import { ContactFormComponent } from './landing-page/contact-form/contact-form.component';
-import { FooterComponent } from './landing-page/footer/footer.component';
-import { InstagramPostsComponent } from './landing-page/instagram-posts/instagram-posts.component';
-import { NavbarComponent } from './landing-page/navbar/navbar.component';
-import { UserLoginStateComponent } from './landing-page/navbar/user-login-state/user-login-state.component';
-import { SearchFormComponent } from './landing-page/search-form/search-form.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './login/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent,
-    CarouselComponent,
-    ContactFormComponent,
-    FooterComponent,
-    InstagramPostsComponent,
-    NavbarComponent,
-    UserLoginStateComponent,
-    SearchFormComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
