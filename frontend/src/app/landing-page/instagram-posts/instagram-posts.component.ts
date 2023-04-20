ef2c32e272b347a20342843bdce4e0f4165b13c6
SzymonWalasik <szymwal050@student.polsl.pl>
2023-04-20 12:35:12 +0200

    struktura komponentów p.2

---
+++ frontend/src/app/landing-page/instagram-posts/instagram-posts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-instagram-posts',
+  templateUrl: './instagram-posts.component.html',
+  styleUrls: ['./instagram-posts.component.sass']
+})
+export class InstagramPostsComponent {
+
+}
