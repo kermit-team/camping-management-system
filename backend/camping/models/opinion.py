32b993d4065c71d374d9196925fb6d12dabc4322
Patryk Beberok <patrol0990@gmail.com>
2023-04-28 22:39:47 +0200

    Added permissions for groups and implemented new models for camping app

---
+++ backend/camping/models/opinion.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.utils.translation import gettext as _
+
+from account.models.user import User
+from .camping_plot import CampingPlot
+
+
+class Opinion(models.Model):
+
+    class Rating(models.IntegerChoices):
+        BAD = 1, _('Bad')
+        DECENT = 2, _('Decent')
+        OK = 3, _('Ok')
+        GREAT = 4, _('Great')
+        AWESOME = 5, _('Awesome')
+    
+    rating = models.IntegerField(choices=Rating.choices)
+    description = models.CharField(default='', blank=True, max_length=500)
+    author = models.ForeignKey(User, on_delete=models.RESTRICT)
+    camping_plot = models.ForeignKey(CampingPlot, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.author.first_name} {self.author.last_name} - {self.description}'
