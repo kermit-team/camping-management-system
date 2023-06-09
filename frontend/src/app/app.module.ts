31ddfa176418118048e1c42b6d1b4dd54624b05b
Katarzyna Szczepańczyk <k.szczepanczyk83@gmail.com>
2023-06-09 16:35:28 +0200

    Minor fix in app modules

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -13,8 +13,6 @@ import { CarouselComponent } from './landing-page/carousel/carousel.component';
 import { ContactFormComponent } from './landing-page/contact-form/contact-form.component';
 import { FooterComponent } from './landing-page/footer/footer.component';
 import { AuthModule } from './login/auth.module';
-import { ProfileMiniComponent } from './shared/profile-mini/profile-mini.component';
-import { HeaderComponent } from './shared/header/header.component';
 import { ProfileComponent } from './profile/profile.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
